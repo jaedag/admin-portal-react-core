ce1e773a7f6cad425fe35906f46fc46a7ce5e5fb
John-Dag Addy <jaedagy@gmail.com>
2023-04-08 15:19:27 +0000

    feat: first build of package

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,20 +1,8 @@
-export declare const getTime: (time: Date) => string;
-export declare const setTime: (timeArray: number[]) => Date;
-export declare const parseTimeToDate: (timeString: string) => string;
-export declare const getMondayThisWeek: (date: Date) => Date;
-export declare const parseNeoTime: (timestamp: string) => string | undefined;
-export declare const parseDate: (date: string) => string;
-export declare const getHumanReadableDate: (date: string, weekday?: true) => string | undefined;
-declare type MemberType = {
-    dob: {
-        date: string;
-    };
-};
-export declare const getMemberDob: (displayMember: MemberType) => string | null | undefined;
-export declare const getWeekNumber: (date?: string) => number;
-export declare const last3Weeks: () => number[];
-export declare const isToday: (date: string) => boolean;
-export declare const getTodayTime: (timeString: string) => string;
-export declare const addHours: (date: string, hours: number) => Date;
-export declare const addMinutes: (date: string, minutes: number) => Date;
-export {};
+export declare const PHONE_NUM_REGEX: RegExp;
+export declare const MOMO_NUM_REGEX: RegExp;
+export declare const DECIMAL_NUM_REGEX: RegExp;
+export declare const DECIMAL_NUM_REGEX_POSITIVE_ONLY: RegExp;
+export declare const USER_PLACEHOLDER = "v1627893621/user_qvwhs7webp";
+export declare const DEBOUNCE_TIMER = 500;
+export declare const LONG_POLL_INTERVAL = 60000;
+export declare const SHORT_POLL_INTERVAL = 5000;
