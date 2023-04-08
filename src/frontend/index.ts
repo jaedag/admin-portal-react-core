753a159194d03e3e9dd44200522f9ca87f76afa2
John-Dag Addy <jaedagy@gmail.com>
2023-04-08 15:02:12 +0000

    feat: initial commit

---
+++ src/frontend/index.ts
@@ -0,0 +1,8 @@
+export const PHONE_NUM_REGEX = /^[+][(]{0,1}[1-9]{1,4}[)]{0,1}[-\s/0-9]*$/
+export const MOMO_NUM_REGEX = /^[0][\s/0-9]{9}$/
+export const DECIMAL_NUM_REGEX = /^-?\d*\.{1}\d*$/
+export const DECIMAL_NUM_REGEX_POSITIVE_ONLY = /^\d*\.{1}\d*$/
+export const USER_PLACEHOLDER = 'v1627893621/user_qvwhs7webp'
+export const DEBOUNCE_TIMER = 500
+export const LONG_POLL_INTERVAL = 60000
+export const SHORT_POLL_INTERVAL = 5000
