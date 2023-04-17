46744626643b431389d99dfe6908b5a48864ff58
John-Dag Addy <jaedagy@gmail.com>
2023-04-17 11:00:50 +0000

    fix:  update rollup config to handle chakra ui

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,8 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import { terser } from '@rollup/plugin-terser'
 import dts from 'rollup-plugin-dts'
 import postcss from 'rollup-plugin-postcss'
 
@@ -10,23 +12,28 @@ export default [
   {
     input: 'src/index.ts',
     external: ['react-dom'],
+    preserveModules: true,
     output: [
       {
         file: packageJson.main,
         format: 'cjs',
         sourcemap: true,
+        exports: 'auto',
       },
       {
         file: packageJson.module,
         format: 'esm',
         sourcemap: true,
+        exports: 'auto',
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       postcss(),
+      terser(),
     ],
   },
   {
