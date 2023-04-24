ef1f0bfd9a56635a369dfa2f86941478e1c56f5e
John-Dag Addy <jaedagy@gmail.com>
2023-04-24 17:21:43 +0000

    fix: update export of initialLoading

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { default as LoadingPage } from './LoadingPage'
+export { default as InitialLoading } from './InitialLoading'
 export { default as ErrorPage } from './ErrorPage'
 export { default as ApolloWrapper } from './ApolloWrapper'
 export { default as PageNotFound } from './PageNotFound'
