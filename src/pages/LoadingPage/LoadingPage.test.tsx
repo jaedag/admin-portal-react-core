d377714c1efcc8b865d77a43a0dcfdd104787e4d
John-Dag Addy <jaedagy@gmail.com>
2023-04-24 17:08:09 +0000

    test: update snapshots for loading page

--- src/pages/LoadingPage/LoadingPage.test.tsx
+++ src/pages/LoadingPage/LoadingPage.test.tsx
@@ -10,10 +10,7 @@ describe('LoadingPage', () => {
     expect(spinner).toBeInTheDocument()
   })
 
-  // TODO: Imlpement test to check whether comopnent was called with correct props
-
-  jest.mock('@chakra-ui/react', () => 'This is PermissionsContainer')
-  test('spinner has the correct size', () => {
+  test('spinner renders correctly', () => {
     const container = render(<LoadingPage />)
     expect(container).toMatchSnapshot()
   })
