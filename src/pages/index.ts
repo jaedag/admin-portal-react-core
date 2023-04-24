f3ef9d05b61a255abed37b395771d0198f3d6a9f
John-Dag Addy <jaedagy@gmail.com>
2023-04-24 10:29:57 +0000

    refactor: move pages folder to root

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as LoadingPage } from './LoadingPage'
 export { default as ErrorPage } from './ErrorPage'
 export { default as ApolloWrapper } from './ApolloWrapper'
+export { default as PageNotFound } from './PageNotFound'
