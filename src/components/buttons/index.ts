80cef84c84ff92d2e505609b2513b5dba661c177
John-Dag Addy <jaedagy@gmail.com>
2023-05-09 21:18:04 +0000

    fix: correct export of EditButton

--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,3 +1,4 @@
 export { default as MenuButton } from './MenuButton'
 export { default as ViewAll } from './ViewAll'
+export { default as EditButton } from './EditButton'
 export { default as ChurchPillButton } from './ChurchPillButton'
