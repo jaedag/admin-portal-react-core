e4c2904144c8e49075f9ebfd99161502d74368da
John-Dag Addy <jaedagy@gmail.com>
2023-05-20 22:24:18 +0000

    fix: update searchChurchCard with options for bacenta

--- src/components/cards/SearchChurchCard/SearchChurchCard.stories.tsx
+++ src/components/cards/SearchChurchCard/SearchChurchCard.stories.tsx
@@ -17,12 +17,31 @@ const Template: StoryFn<SearchChurchCardPropsInterface> = (args) => (
   <SearchChurchCard {...args} />
 )
 
-export const Default = Template.bind({})
-Default.args = {
+export const WithFellowship = Template.bind({})
+WithFellowship.args = {
   church: {
     id: '1',
     name: 'Greater Love',
-    levelName: 'MegaBacenta',
+    levelName: 'Fellowship',
+    __typename: 'Fellowship',
+    leader: {
+      id: '1',
+      firstName: 'Dan',
+      lastName: 'Abramov',
+      nameWithTitle: 'Ps. Dan Abramov',
+      pictureUrl: 'https://bit.ly/dan-abramov',
+    },
+  } as Church,
+  onClick: () => alert('Card clicked'),
+}
+
+export const WithBacenta = Template.bind({})
+WithBacenta.args = {
+  church: {
+    id: '1',
+    name: 'Greater Love',
+    levelName: 'Bacenta',
+    __typename: 'Bacenta',
     leader: {
       id: '1',
       firstName: 'Dan',
--- src/components/cards/SearchChurchCard/SearchChurchCard.tsx
+++ src/components/cards/SearchChurchCard/SearchChurchCard.tsx
@@ -1,11 +1,51 @@
 import React from 'react'
-import { Avatar, Box, Card, CardBody, Flex, Text } from '@chakra-ui/react'
+import {
+  Avatar,
+  AvatarBadge,
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Text,
+} from '@chakra-ui/react'
 import { getCloudinaryPreset } from '@/utils'
 import { Church } from '@/types'
+import { BsPeopleFill } from 'react-icons/bs'
+import { FaBus } from 'react-icons/fa'
 
 export interface SearchChurchCardPropsInterface {
   church: Church
   onClick?: () => void
+}
+
+const SearchIconBadge = ({ church }: { church: Church }) => {
+  if (church.__typename === 'Fellowship') {
+    return (
+      <AvatarBadge
+        borderColor="papayawhip"
+        bg="tomato"
+        boxSize="1.25em"
+        padding={1}
+      >
+        <BsPeopleFill color="white" />
+      </AvatarBadge>
+    )
+  }
+
+  if (church.__typename === 'Bacenta') {
+    return (
+      <AvatarBadge
+        borderColor="blue.200"
+        bg="blue.600"
+        boxSize="1.25em"
+        padding={1}
+      >
+        <FaBus color="white" />
+      </AvatarBadge>
+    )
+  }
+
+  return <div>SearchIconBadge</div>
 }
 
 const SearchChurchCard = (props: SearchChurchCardPropsInterface) => {
@@ -13,7 +53,7 @@ const SearchChurchCard = (props: SearchChurchCardPropsInterface) => {
 
   return (
     <Card
-      data-testid="search-member-card"
+      data-testid="search-church-card"
       direction="row"
       marginY={2}
       onClick={onClick}
@@ -22,10 +62,13 @@ const SearchChurchCard = (props: SearchChurchCardPropsInterface) => {
         <Flex gap="5" alignContent="center" alignItems="center">
           <Avatar
             data-testid="avatar"
+            borderRadius={10}
             name={church.name + ' ' + church.levelName}
             size="lg"
             src={getCloudinaryPreset(church.leader.pictureUrl)}
-          />
+          >
+            <SearchIconBadge church={church} />
+          </Avatar>
           <Box>
             <Text marginBottom={0} fontWeight="bold" fontSize="lg">
               {church.name + ' ' + church.levelName}
