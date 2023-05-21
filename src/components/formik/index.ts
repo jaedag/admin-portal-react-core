8de38dea37652698c3ebdd20fdde2b4a89ede0fc
John-Dag Addy <jaedagy@gmail.com>
2023-05-21 06:45:57 +0000

    feat: implement submitButton

--- src/components/formik/index.ts
+++ src/components/formik/index.ts
@@ -1,3 +1,4 @@
 export { default as Input } from './Input'
 export { default as Select } from './Select'
 export { default as ImageUpload } from './ImageUpload'
+export { default as SubmitButton } from './SubmitButton'
