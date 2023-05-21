523c6c061e8f66b7a9939a65d1eb295b990276ac
John-Dag Addy <jaedagy@gmail.com>
2023-05-21 16:39:37 +0000

    feat: remove api folder

--- api/README.md
+++
@@ -1,7 +0,0 @@
-
- # README 
-
-Tutorial followed:
-
-Install these packages to get started so that you can use the Chakra UI components in your library:
-
--- api/src/utils/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from '@/types'
-
-export const isAuth = (permittedRoles: Role[], userRoles?: Role[]) => {
-  if (!permittedRoles.some((r) => userRoles?.includes(r))) {
-    throw new Error('You are not permitted to run this mutation')
-  }
-}
