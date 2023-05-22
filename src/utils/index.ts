1fc4a039f018ed54c69603c7fbea7b2c57b3a163
John-Dag Addy <jaedagy@gmail.com>
2023-05-22 22:03:28 +0000

    fix: add regex for some fields

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './helper-functions'
 export * from './date-functions'
 export * from './payment-fns'
+export * from './regex'
---
+++ src/utils/regex.ts
@@ -0,0 +1,4 @@
+export const PHONE_NUM_REGEX = /^[+][(]{0,1}[1-9]{1,4}[)]{0,1}[-\s/0-9]*$/
+export const MOMO_NUM_REGEX = /^(0[1-9]\d{8}|[1-9]\d{9})$/
+export const DECIMAL_NUM_REGEX = /^-?\d*\.{1}\d*$/
+export const DECIMAL_NUM_REGEX_POSITIVE_ONLY = /^\d*\.{1}\d*$/
