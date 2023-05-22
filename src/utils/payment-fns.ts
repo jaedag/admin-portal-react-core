52e1448899b3af288e230421eff4ceff4d176dc8
John-Dag Addy <jaedagy@gmail.com>
2023-05-22 21:48:39 +0000

    fix: add constant values for GH_MOMO_NETWORKS

---
+++ src/utils/payment-fns.ts
@@ -0,0 +1,8 @@
+import { FormikSelectOptions } from '@/components/formik/formik-types'
+
+export const GH_MOBILE_NETWORK_OPTIONS: FormikSelectOptions = [
+  { key: '', value: '' },
+  { key: 'MTN', value: 'MTN' },
+  { key: 'Vodafone', value: 'Vodafone' },
+  { key: 'AirtelTigo', value: 'AirtelTigo' },
+]
