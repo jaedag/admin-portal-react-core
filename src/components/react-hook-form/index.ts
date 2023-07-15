e3565dbdd750d2c335664a093b25d3cd6b5825bb
John-Dag Addy <jaedagy@gmail.com>
2023-07-15 18:57:36 +0000

    fix: export ReactHookFormComponentProps

--- src/components/react-hook-form/index.ts
+++ src/components/react-hook-form/index.ts
@@ -1,3 +1,5 @@
 export { default as Input } from './Input'
 export { default as Select } from './Select'
 export { default as ImageUpload } from './ImageUpload'
+
+export * from './react-hook-form-types'
